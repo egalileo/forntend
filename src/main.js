2e55116025d26cb231507e0bbe469f59ce2d9218
Erick Galileo Padilla <61204839+egalileo@users.noreply.github.com>
2022-09-08 18:32:57 -0600

    main

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import '@babel/polyfill'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+//url del backend recuerda quitar cualquier pleca del final / quitarla
+//axios.defaults.baseURL= process.env.BACKEND || "https://backend.alexandercalderon.me"
+//axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
+axios.defaults.baseURL= process.env.BACKEND || "http://localhost:3000"
+
+
+Vue.use(VueAxios, axios)
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
